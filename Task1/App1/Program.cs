27692b6e770f6158dadc62a1fcd314e734fab626
Dishon Wambua <48084263+dishonwambu@users.noreply.github.com>
2023-06-24 15:36:04 +0300

    initial commits

--- Task1/App1/Program.cs
+++ Task1/App1/Program.cs
@@ -1,15 +1,5 @@
 ﻿using App1;
 
-//internal class Program
-//{
-// private static void Main(string[] args)
-// {
-//  EmployeeDataAccessTests tests = new EmployeeDataAccessTests();
-//  tests.TestUpdateEmployeeSalary();
-//creating an instance of the EmployeeDataAccessTests class and calling
-//the TestUpdateEmployeeSalary method to execute the unit test.
-//    }
-//}
 using NUnitLite;
 
 class Program
